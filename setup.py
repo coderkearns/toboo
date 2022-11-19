7ff38827efe1e69bfeea1e77ee11dd3334f82819
coderkearns <coder.kearns@gmail.com>
2022-11-19 17:32:36 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="toboo",
+    version="1.0",
+    description="The github.com/coderkearns/toboo todo app/game.",
+    author="coderkearns",
+    packages=["toboo"],
+)
